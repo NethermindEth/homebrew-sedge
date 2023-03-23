a03ed91c04f249f9f2f2e14046f85b6aa5e6475b
adriantpaez <adriantpaez@gmail.com>
2023-03-23 15:05:46 +0100

    fix: remove repeated v prefix from version

--- Formula/sedge.rb
+++ Formula/sedge.rb
@@ -7,13 +7,13 @@ class Sedge < Formula
 
     case
     when OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/NethermindEth/sedge/releases/download/v#{app_version}/sedge-v#{app_version}-darwin-amd64"
+      url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-darwin-amd64"
       sha256 "54add957180bc985f2c263c206f02be478762882e33f178e61fddb93c062e682"
     when OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/NethermindEth/sedge/releases/download/v#{app_version}/sedge-v#{app_version}-darwin-arm64"
+      url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-darwin-arm64"
       sha256 "c1a02fc7986fa140a3d524f93b56904534548a1c6a24f7ef3a3f4826de767995"
     when OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/NethermindEth/sedge/releases/download/v#{app_version}/sedge-v#{app_version}-linux-amd64"        
+      url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-linux-amd64"        
       sha256 "64ae1ad57a2002856ce7a4d2e49e36d85234dcd8d4b4a1edc017df4f2949d81e"
     else
       odie "Unexpected platform!"
@@ -21,13 +21,13 @@ class Sedge < Formula
 
     def install
       if OS.mac? && Hardware::CPU.intel?
-        File.rename("./sedge-v#{version}-darwin-amd64","./sedge")
+        File.rename("./sedge-#{version}-darwin-amd64","./sedge")
       end
       if OS.mac? && Hardware::CPU.arm?
-        File.rename("./sedge-v#{version}-darwin-arm64","./sedge")
+        File.rename("./sedge-#{version}-darwin-arm64","./sedge")
       end
       if OS.linux? && Hardware::CPU.intel?
-        File.rename("./sedge-v#{version}-linux-amd64","./sedge")
+        File.rename("./sedge-#{version}-linux-amd64","./sedge")
       end
       bin.install Dir['./sedge']
     end
