d41d7b9323b84464eea0c09faea79647059482fc
stdevMac <stdevMac@users.noreply.github.com>
2024-03-08 16:14:30 +0000

    Update Homebrew to latest release

--- Formula/sedge.rb
+++ Formula/sedge.rb
@@ -1,5 +1,5 @@
 class Sedge < Formula
-    app_version = 'v1.3.1'
+    app_version = 'v1.3.2'
     desc "A one click setup tool for PoS network/chain validators."
     homepage "https://docs.sedge.nethermind.io/"
     license "Apache License 2.0"
@@ -8,13 +8,13 @@ class Sedge < Formula
     case
     when OS.mac? && Hardware::CPU.intel?
       url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-darwin-amd64"
-      sha256 "cb1ae5f4c5577d36fa25d5a9484acd480e867749960f0b56245b373dd1d3cb60"
+      sha256 "573e38d2de82cfd3c77e6799f4b2917f0f1d7aaeb0537c1b706d49c0c5f80438"
     when OS.mac? && Hardware::CPU.arm?
       url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-darwin-arm64"
-      sha256 "fd25faf05b5ad19b4b01894928f06a1c73c3f431f8b7e87f0826fa1604614b1a"
+      sha256 "7e42cf3ad1774053687da1bbc6c670559da0e653fd96ef351249ebe79002e5cb"
     when OS.linux? && Hardware::CPU.intel?
       url "https://github.com/NethermindEth/sedge/releases/download/#{app_version}/sedge-#{app_version}-linux-amd64"        
-      sha256 "5d5a3451afc671dc1244e6d5951f34780a6b1f3b6ad36e41eac4915c8d114532"
+      sha256 "6bb8d6d902e3cfb841093d3305a2123a85d4143df62d778ac1680be7aaae7996"
     else
       odie "Unexpected platform!"
     end
